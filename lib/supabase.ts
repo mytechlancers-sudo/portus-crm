1ef728db5617c30623858a7ed273e582add0efff
Yaseen Irfan <yaseen@portuspk.com>
2026-05-19 15:04:11 +0500

    fix: hardcode fallback keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://zapybyfohfjytyjzkhrn.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_8sIG7KArM9GaX63hsxdcFg_8e5B5w2V'
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY || 'sb_secret_zA_Jcg5LPZ6TRA41LPb4VQ_hdguroTj'
 
-export const supabase = createClient(url, key)
-export const supabaseAdmin = createClient(url, process.env.SUPABASE_SERVICE_ROLE_KEY!)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey)
 export default supabase
